1fba3d0f1070380ee85ea20b62c35e4447fb42ea
joekamien <joekamien@users.noreply.github.com>
2015-10-12 09:45:50 -0700

    Add code to generate and print solution.

--- RDP-2015-10-12-Tetris/Program.cs
+++ RDP-2015-10-12-Tetris/Program.cs
@@ -10,6 +10,37 @@ namespace RDP_2015_10_12_Tetris
     {
         static void Main(string[] args)
         {
+            Bag bag = new Bag();
+            for(int i = 0; i < 50; i++)
+            {
+                Console.Write(bag.next());
+            }
+            Console.WriteLine();
         }
+    }
+
+    class Bag
+    {
+        private Stack<char> stack = new Stack<char>(7);
+        private Random rng = new Random();
+
+        public char next()
+        {
+            if (stack.Count() == 0) replenish();    
+
+            return stack.Pop();
+        }
+
+        public void replenish()
+        {
+            List<char> ordered = new List<char>(new char[] { 'O', 'I', 'S', 'Z', 'L', 'J', 'T' });
+            while (ordered.Count() > 0)
+            {
+                int pos = rng.Next(0, ordered.Count());
+                stack.Push(ordered[pos]);
+                ordered.RemoveAt(pos);
+            }
+        }
+
     }
 }
